f9b1a6d23b26d05e3eb6c27e620a541d380e0bef
kamlendu08 <kamlendubala@gmail.com>
2025-01-21 21:23:05 +0530

    second

--- lua/community.lua
+++ lua/community.lua
@@ -6,9 +6,11 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.pack.typescript-all-in-one" },
   { import = "astrocommunity.icon.mini-icons" },
+  { import = "astrocommunity.colorscheme.gruvbox-nvim" },
+  { import = "astrocommunity.color.ccc-nvim" },
+  { import = "astrocommunity.pack.typescript-all-in-one" },
   { import = "astrocommunity.pack.cpp" },
-  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.file-explorer.telescope-file-browser-nvim" },
   -- import/override with your plugins folder
 }
